64b26c3d11bef1f023970302429dcd21d44b9cd2
Nabiev Asliddin <aslnabiev2002@gmail.com>
2024-08-29 05:02:49 +0500

    [features]update file configs.go

--- configs/configs.go
+++ configs/configs.go
@@ -13,19 +13,19 @@ var AppSettings models.Configs
 
 func ReadString() error {
 	fmt.Println("Starting reading settings file")
-	configfile, err := os.Open("configs/configs.json")
+	configFile, err := os.Open("configs/configs.json")
 	if err != nil {
 		return errors.New(fmt.Sprintf("Couldn't open config file. Error is: %s", err.Error()))
 	}
-	defer func(configfile *os.File) {
-		err = configfile.Close()
+	defer func(configFile *os.File) {
+		err = configFile.Close()
 		if err != nil {
 			log.Fatal("Couldn't close config file. Error is: ", err.Error())
 		}
 
-	}(configfile)
+	}(configFile)
 	fmt.Println("Starting decoding settings file")
-	err = json.NewDecoder(configfile).Decode(&AppSettings)
+	err = json.NewDecoder(configFile).Decode(&AppSettings)
 	if err != nil {
 		return errors.New(fmt.Sprintf("Couldn't decode settings json file. Error is: %s", err.Error()))
 	}
